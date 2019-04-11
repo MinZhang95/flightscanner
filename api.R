d56f552192e2a4153dfbc22dbc1d07075876ac89
qiaoyang00 <37715980+qiaoyang00@users.noreply.github.com>
2019-04-11 04:51:40 -0500

    Update api.R

--- api.R
+++ api.R
@@ -2,44 +2,127 @@
 library(dplyr)
 library(httr)
 
+SetAPI <- function(host, key) {
+  options(API = list(host = host, key = key))
+}
+
+
+MakeHeader <- function(host, key, type) {
+  if (missing(host)) host <- getOption("API")$host
+  if (missing(key)) key <- getOption("API")$key
+  if (missing(type)) type <- "application/x-www-form-urlencoded"
+  c("X-RapidAPI-Host" = host, "X-RapidAPI-Key" = key, "Content-Type" = type)
+}
+
+
+CreateSession <- function(origin, destination, startDate, returnDate = NULL,
+                          adults = 1, children = NULL, infants = NULL,
+                          country = "US", currency = "USD", locale = "en-US",
+                          cabinClass = "economy", includeCarriers = NULL, excludeCarriers = NULL) {
+  # Add checking here.
+
+  url <- paste0("https://", getOption("API")$host, "/apiservices/pricing/v1.0")
+  header <- MakeHeader()
+  query <- list("cabinClass" = cabinClass,
+                "country" = country,
+                "currency" = currency,
+                "locale" = locale,
+                "originPlace" = paste0(origin, "-sky"),
+                "destinationPlace" = paste0(destination, "-sky"),
+                "outboundDate" = startDate,
+                "inboundDate" = returnDate,
+                "adults" = adults,
+                "children" = children,
+                "infants" = infants,
+                "includeCarriers" = includeCarriers,
+                "excludeCarriers" = excludeCarriers)
+
+  resp <- POST(url, add_headers(header), body = query, encode = "form")
+  flag <- CheckStatusCode(resp)
+  resp
+}
+
+
+PollSession <- function(sessionKey, respondPOST = NULL) {
+  if (missing(sessionKey))
+    sessionKey <- SessionKey(respondPOST)
+
+  url <- paste0("https://", getOption("API")$host, "/apiservices/pricing/uk2/v1.0")
+  header <- MakeHeader()
+  path <- c(parse_url(url)$path, sessionKey)
+
+  resp <- GET(url, add_headers(header), path = path)
+  flag <- CheckStatusCode(resp)
+  resp
+}
+
+
+BrowseFlight <- function(endpoint = c("quotes", "routes", "dates"),
+                         origin, destination, startDate, returnDate = NULL,
+                         country = "US", currency = "USD", locale = "en-US") {
+  endpoint <- match.arg(endpoint)
+  # Add checking here.
+
+  url <- paste0("https://", getOption("API")$host, "/apiservices/browse", endpoint, "/v1.0")
+  header <- MakeHeader()
+  path <- c(parse_url(url)$path, country, currency, locale,
+            paste0(origin, "-sky"), paste0(destination, "-sky"), startDate)
+  query <- list("inboundpartialdate" = returnDate)
+
+  resp <- GET(url, add_headers(header), path = path, query = query)
+  flag <- CheckStatusCode(resp)
+  resp
+}
+
+
+
+# return 1 if error, else 0.
+CheckStatusCode <- function(x) {
+  code <- status_code(x)
+  if (http_error(x)) {
+    warning("Status Code ", code, ": Request failed.")
+    switch(as.character(code),
+           "400" = "",
+           "401" = "",
+           "402" = "",
+           "404" = "",
+           "405" = "",
+           "406" = "",
+           "407" = "",
+           "408" = "",
+           "409" = "",
+           "410" = "",
+           "415" = "",
+           "500" = "")
+    1
+  } else 0
+}
+
+
+SessionKey <- function(x) {
+  location <- headers(x)$location
+  last(strsplit(location, "/")[[1]])
+}
+
+
+
+
+########################################
+########################################
 # header information
-host <- "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-key <- "3e85a0e43cmshac6dba6fde57066p1c1145jsn1e6f8c3d0e33"  # 50/day
-type <- "application/x-www-form-urlencoded"
+SetAPI(host = "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+       key = "3e85a0e43cmshac6dba6fde57066p1c1145jsn1e6f8c3d0e33")
+getOption("API")
 
 
 ### Create session - Live Flight Search
-url.post <- "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0"
-header <- c("X-RapidAPI-Host" = host,
-            "X-RapidAPI-Key" = key,
-            "Content-Type" = type)
-
-query <- list("cabinClass" = "economy",
-              "children" = 0,
-              "infants" = 0,
-              "country" = "US",
-              "currency" = "USD",
-              "locale" = "en-US",
-              "originPlace" = "DSM-sky",
-              "destinationPlace" = "DTW-sky",
-              "outboundDate" = "2019-05-01",
-              "adults" = 1)
-# body <- stringr::str_split(modify_url("", query = query), "\\?")[[1]][2]
-
-resp.post <- POST(url.post, add_headers(.headers = header), body = query, encode = "form")
+resp.post <- CreateSession(orig = "DSM", dest = "DTW", startDate = "2019-05-01", returnDate = NULL)
 resp.post
-resp.post$request
-headers(resp.post)
-glimpse(resp.post)
-content(resp.post)
+SessionKey(resp.post)
 
 
 ### Poll session - Live Flight Search
-url.get <- "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/"
-key.session <- stringr::word(resp.post$headers$location, -1, sep = "/")
-# "8c38fc31-37fc-4fbe-bdb7-26348f5a79fe"
-
-resp.get <- GET(url.get, add_headers(.headers = header), path = paste0(parse_url(url.get)$path, key.session))
+resp.get <- PollSession(respondPOST = resp.post)
 resp.get
 resp.get$request
 headers(resp.get)
@@ -58,19 +141,25 @@ res$Places[[1]]
 res$Currencies[[1]]
 
 
-
-
-
-
+### Browse Quotes - Browse Flight Prices
+resp.quote <- BrowseFlight("quotes", orig = "DSM", dest = "DTW", start = "2019-05-01", return = NULL)
+resp.quote
+headers(resp.quote)
+glimpse(resp.quote)
+content(resp.quote)
 
 
 ### Browse Routes - Browse Flight Prices
-url <- "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/DSM-sky/DTW-sky/2019-05-01"
-header <- c("X-RapidAPI-Host" = host,
-            "X-RapidAPI-Key" = key)
+resp.route <- BrowseFlight("routes", orig = "DSM", dest = "DTW", start = "2019-05-01", return = NULL)
+resp.route
+headers(resp.route)
+glimpse(resp.route)
+content(resp.route)
 
-resp <- GET(url, add_headers(.headers = header))
-resp
-headers(resp)
-glimpse(resp)
-content(resp)
+
+### Browse Dates - Browse Flight Prices
+resp.date <- BrowseFlight("dates", orig = "DSM", dest = "DTW", start = "2019-05-01", return = NULL)
+resp.date
+headers(resp.date)
+glimpse(resp.date)
+content(resp.date)

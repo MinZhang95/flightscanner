e97d71efd0b8ed247d36db8967accdb3430a0406
zeruiz <zedray.chang@gmail.com>
2019-04-28 11:09:15 -0500

    Change the time from download_data

--- R/download_data.R
+++ R/download_data.R
@@ -47,5 +47,15 @@ download_data<- function(trip_type, from, to, date){
       flight %>%
         unnest(PricingOptions,.drop=FALSE) %>% 
         unnest(CarrierInfo,.drop=FALSE)  -> res
+      
+      ## Change POSIXct type of time in Out_DepartureTime, Out_ArrivalTime, In_DepartureTime,
+      ## In_ArrivalTime to period type.
+      res %>% mutate(Out_DepartureTime = format(Out_DepartureTime, format="%H:%M:%S") %>% hms(),
+                     Out_ArrivalTime = format(Out_ArrivalTime, format="%H:%M:%S") %>% hms()) -> res
+      ifelse(trip_type != 1,
+        res %>% mutate(In_DepartureTime = format(In_DepartureTime, format="%H:%M:%S") %>% hms(),
+                       In_ArrivalTime = format(In_ArrivalTime, format="%H:%M:%S") %>% hms()) -> res,
+        break
+      )
       return(res)
 }

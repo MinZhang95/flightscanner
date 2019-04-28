2082f76591881468bb1d3436abccd42ffbe4b97a
dapengh <shenfeng9224@gmail.com>
2019-04-28 16:14:58 -0500

    solving the notes that lacks global variable

--- R/download_data.R
+++ R/download_data.R
@@ -6,48 +6,49 @@
 #' @param date date
 #' @import dplyr
 #' @import tidyr
+#' @importFrom rlang .data
 #' @return a data frame
 #' @export
 #'
 download_data<- function(trip_type, from, to, date){
       if(trip_type==1) {
-        resp.post <- CreateSession(orig = from, 
-                                   dest = to, 
+        resp.post <- CreateSession(origin = from, 
+                                   destination = to, 
                                    startDate = date[1] )
       }else{
-        resp.post <- CreateSession(orig = from, 
-                                   dest = to, 
+        resp.post <- CreateSession(origin = from, 
+                                   destination = to, 
                                    startDate = date[1], 
                                    returnDate = date[2] )
       }
       resp.get <- PollSession(respondPOST = resp.post)
       data <- GetData(resp.get)
-      Out <- data$legs %>% filter(Directionality == 'Outbound') %>% 
+      Out <- data$legs %>% filter(.data$Directionality == 'Outbound') %>% 
         select("Id","DepartureTime","ArrivalTime","Duration","No.Stops","Stops") %>% 
         magrittr::set_names(c("OutboundLegId","Out_DepartureTime","Out_ArrivalTime","Out_Duration","Out_No.Stops","Out_Stops"))
-      In <- data$legs %>% filter(Directionality == 'Inbound') %>% 
+      In <- data$legs %>% filter(.data$Directionality == 'Inbound') %>% 
         select("Id","DepartureTime","ArrivalTime","Duration","No.Stops","Stops") %>% 
         magrittr::set_names(c("InboundLegId","In_DepartureTime","In_ArrivalTime","In_Duration","In_No.Stops","In_Stops"))
       seg_carrier <- left_join(data$segments %>% 
-                                 select(Id, CarrierId),
+                                 select(.data$Id, .data$CarrierId),
                                data$carriers %>% 
-                                 select(Id,Code,Name) %>% 
+                                 select(.data$Id, .data$Code, .data$Name) %>% 
                                  magrittr::set_names(c("CarrierId",'Code','Name')),by = "CarrierId")
       
-      segment_info <- data$legs %>% select(Id,SegmentIds) %>% 
-        unnest(SegmentIds) %>% 
+      segment_info <- data$legs %>% select(.data$Id, .data$SegmentIds) %>% 
+        unnest(.data$SegmentIds) %>% 
         left_join(seg_carrier,by=c("SegmentIds"= "Id")) %>% 
-        select(Id, CarrierId,Code,Name) %>% 
+        select(.data$Id, .data$CarrierId,.data$Code,.data$Name) %>% 
         magrittr::set_names(c("LegId","CarrierId","CarrierCode","CarrierName")) %>% 
-        nest(-LegId) %>% magrittr::set_names(c('LegId','CarrierInfo'))
+        nest(-.data$LegId) %>% magrittr::set_names(c('LegId','CarrierInfo'))
       flight <- data$itineraries %>% 
-        left_join(Out,by="OutboundLegId") %>% 
-        left_join(In,by="InboundLegId") %>% 
-        left_join(data$price %>% select(-SearchTime),by=c("OutboundLegId","InboundLegId")) %>% 
+        left_join(.data$Out,by="OutboundLegId") %>% 
+        left_join(.data$In,by="InboundLegId") %>% 
+        left_join(data$price %>% select(-.data$SearchTime),by=c("OutboundLegId","InboundLegId")) %>% 
         left_join(segment_info,by=c("OutboundLegId"="LegId", "InboundLegId"="LegId"))
       flight %>%
-        unnest(PricingOptions,.drop=FALSE) %>% 
-        unnest(CarrierInfo,.drop=FALSE)  -> res
+        unnest(.data$PricingOptions,.drop=FALSE) %>% 
+        unnest(.data$CarrierInfo,.drop=FALSE)  -> res
       
       ## Change POSIXct type of time in Out_DepartureTime, Out_ArrivalTime, In_DepartureTime,
       ## In_ArrivalTime to period type.

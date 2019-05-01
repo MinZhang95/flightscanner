910b06e9bc05ed56bf8546fb2d4c524dd4cad850
N.A.C <allenansa@gmail.com>
2019-05-01 16:57:12 -0500

    update shiny

--- inst/sql.R
+++ inst/sql.R
@@ -11,15 +11,16 @@ pkgdown::build_site()
 con <- dbCreateDB(dbname = "inst/flight.db")
 con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "inst/flight.db")
 # dbDisconnect(con)
-dbListTables(con)
+DBI::dbListTables(con)
 
 # remove all tables
-sapply(dbListTables(con), dbRemoveTable, conn = con)
+sapply(DBI::dbListTables(con), DBI::dbRemoveTable, conn = con)
 
 # write data into database
 SetAPI(host = "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
        key = "3e85a0e43cmshac6dba6fde57066p1c1145jsn1e6f8c3d0e33")
-resp.get <- CreateSession(orig = "SEA", dest = "PVG", startDate = "2019-06-01", returnDate = "2019-07-01") %>%
+resp.get <- CreateSession(orig = "SEA", dest = "PVG",
+                          startDate = "2019-06-01", returnDate = "2019-07-01") %>%
   PollSession(respondPOST = .)
 SaveData(con, x = resp.get)
 
@@ -29,11 +30,12 @@ data <- GetData(con)  # from SQLite database
 GetData(con, lazy = T)
 
 # check duplicate Id
-sapply(data, CheckDuplicate)  # Leg: 往返时会重复. carriers: Id = 0重复
+sapply(data, flightscanner:::CheckDuplicate)  # Leg: 往返时会重复. carriers: Id = 0重复
 
 # filter flight
-FilterFlight(data, max_price = 1000, max_duration = 60 * 24,
-             max_stops = 2, layover = c(60, 180),
+a = FilterFlight(data)
+a = FilterFlight(data, max_price = Inf, max_duration = 60 * 24,
+             max_stops = Inf, layover = c(60, 180),
              carrier_include = c("UA", "AA", "DL", "CX", "NH", "MU", "HU"),
              carrier_exclude = c("MH", "KE"))
 
--- shiny/fligthscanner/server.R
+++ shiny/fligthscanner/server.R
@@ -28,114 +28,64 @@ shinyServer(function(input, output,session) {
     input$goButton, 
     {withProgress({
       setProgress(message = "Working Really Hard...")
-      if(input$trip_type==1){
-        try(download_data(input$trip_type,
-                          toupper(input$from),
-                          toupper(input$to),
-                          input$date1))
-      }else{
-        try(download_data(input$trip_type,
-                          toupper(input$from),
-                          toupper(input$to),
-                          c(input$date2,input$date3)))
-      }
+      resp.get <- CreateSession(orig = input$from, dest = input$to,
+                                startDate = input$date1, 
+                                returnDate = input$data2) %>%
+        PollSession(respondPOST = .)
+      GetData(resp.get)
     })
   })
 
   
   output$ui <- renderUI({ 
-    if(class(dataset())[3]=='data.frame'){
       wellPanel(
-        sliderInput("price", label = "Price ($)", min = min(dataset()$Price),
-                    max = max(dataset()$Price), 
-                    value = max(dataset()$Price),ticks = FALSE)
+        sliderInput("price", label = "Price (USD)", 
+                    min = data$price$PricingOptions %>%
+                      sapply(function(x) min(x$Price)) %>% min,
+                    max = data$price$PricingOptions %>%
+                      sapply(function(x) max(x$Price)) %>% max, 
+                    value = data$price$PricingOptions %>%
+                      sapply(function(x) max(x$Price)) %>% max,
+                    ticks = FALSE)
         ,
         tabsetPanel(
           tabPanel("Incl. Airline",
                    selectInput("Airline_In", label = '',
-                               choices = unique(dataset()$CarrierName),
-                               selected = unique(dataset()$CarrierName),
+                               choices = unique(dataset()$carriers$Code),
+                               selected = unique(dataset()$carriers$Code),
                                multiple = TRUE)),
           tabPanel("Excl. Airline",
                    selectInput("Airline_Ex", label = "",
-                               choices = unique(dataset()$CarrierName),
+                               choices = unique(dataset()$carriers$Code),
                                multiple = TRUE)))
         ,
+        sliderInput("Duration",
+                    label = "Duration (hours)",ticks = FALSE,step = 0.1,
+                    min = round((dataset()$legs$Duration %>% min)/60,1),
+                    max = round((dataset()$legs$Duration %>% max)/60,1),
+                    value = round((dataset()$legs$Duration %>% max)/60),1),
+        radioButtons("Back_Stops", label = 'Stops',
+                     choices =list("Nonstops only" = 0, "1 stop or fewer" = 1, "2 stops or fewer" = 2, "I don't care"=Inf),
+                     selected = Inf
+        ),
         tabsetPanel(
           tabPanel("Leave",
                    br(),
-                   sliderInput("Leave_Duration",
-                               label = "Duration (hours)",ticks = FALSE,step = 0.1,
-                               min = round(min(dataset()$Out_Duration)/60,1), 
-                               max = round(max(dataset()$Out_Duration)/60,1), 
-                               value = round(max(dataset()$Out_Duration)/60),1),
-                   radioButtons("Leave_Stops", label = 'Stops',
-                                choices = list("Nonstops only" = 0, "1 stop or fewer" = 1, "2 stops or fewer" = 2, "I don't care"=Inf),
-                                selected = Inf),
-                   sliderInput("Leave_Dep_Time", label = "Departure Time",step = 0.5,ticks = FALSE,
+                     sliderInput("Leave_Dep_Time", label = "Departure Time",step = 0.5,ticks = FALSE,
                                min = 0, max = 24, value = c(0, 24)),
                    sliderInput("Leave_Arr_Time", label = "Arrival Time",step = 0.5,ticks = FALSE,
                                min = 0, max = 24, value = c(0, 24))),
           tabPanel("Back",
                    br(),
-                   sliderInput("Back_Duration",
-                               label = "Duration (hours)",ticks=F,step = 0.1,
-                               min = round(min(dataset()$In_Duration)/60,1), 
-                               max = round(max(dataset()$In_Duration)/60,1),
-                               value = round(max(dataset()$In_Duration)/60,1)),
-                   radioButtons("Back_Stops", label = 'Stops', 
-                                choices =list("Nonstops only" = 0, "1 stop or fewer" = 1, "2 stops or fewer" = 2, "I don't care"=Inf),
-                                selected = Inf
-                   ),
                    sliderInput("Back_Dep_Time", label = "Departure Time",step = 0.5,ticks = FALSE,
                                min = 0, max = 24, value = c(0, 24)),
                    sliderInput("Back_Arr_Time", label = "Arrival Time",step = 0.5,ticks = FALSE,
                                min = 0, max = 24, value = c(0, 24)))
-        )
-      )
-    }else{
-      wellPanel(
-        sliderInput("price", label = "Price ($)", min = 0,
-                    max = Inf, value = 0,ticks = FALSE)
-        ,
-        tabsetPanel(
-          tabPanel("Incl. Airline",
-                   selectInput("Airline_In", label = '',
-                               choices = list(),
-                               multiple = TRUE)),
-          tabPanel("Excl. Airline",
-                   selectInput("Airline_Ex", label = "",
-                               choices = list(),
-                               multiple = TRUE)))
-        ,
-        tabsetPanel(
-          tabPanel("Leave",
-                   br(),
-                   sliderInput("Leave_Duration",
-                               label = "Durationn (hours)",ticks = FALSE,
-                               min = 0, max = Inf, value = 0),
-                   radioButtons("Leave_Stops", label = 'Stops',
-                                choices = list("Enter Something" = 0),
-                                selected = 0),
-                   sliderInput("Leave_Dep_Time", label = "Departure Time",step = 0.5,ticks = FALSE,
-                               min = 0, max = 24, value = c(0, 24)),
-                   sliderInput("Leave_Arr_Time", label = "Arrival Time",step = 0.5,ticks = FALSE,
-                               min = 0, max = 24, value = c(0, 24))),
-          tabPanel("Back",
-                   br(),
-                   sliderInput("Back_Duration",
-                               label = "Duration (hours)",ticks = FALSE,
-                               min = 0, max = Inf, value = 0),
-                   radioButtons("Back_Stops", label = 'Stops',
-                                choices = list("没有我，哈哈，傻逼了吧；）" = 0),
-                                selected = 0),
-                   sliderInput("Back_Dep_Time", label = "Departure Time",step = 0.5,ticks = FALSE,
-                               min = 0, max = 24, value = c(0, 24)),
-                   sliderInput("Back_Arr_Time", label = "Arrival Time",step = 0.5,ticks = FALSE,
-                               min = 0, max = 24, value = c(0, 24)))
-        )
-      )
-    }
+        
+      ))
+    
+       
+      
     
   })
   
@@ -160,8 +110,7 @@ shinyServer(function(input, output,session) {
  
    output$Search_res <- renderText({
     t<- dataset()
-    
-    if(!grepl("tbl",as.character(class(t)[2]))){
+    if(!is.list(t)){
       "Error happened: Check the input information and try again!"
     }else{
       "Search Successed! Click Flight Tab For More Details:)"
@@ -169,55 +118,8 @@ shinyServer(function(input, output,session) {
   })
   
    output$table <- renderDataTable({
-     if(input$trip_type==1){
-       # filter for single trip
-       dataset() %>% 
-         select(-c(Out_Stops, In_Stops)) %>% 
-         filter(Out_No.Stops <= input$Leave_Stops,
-                Out_Duration <= input$Leave_Duration * 60, 
-                hour(Out_DepartureTime)+minute(Out_DepartureTime)/60 >= input$Leave_Dep_Time[1], 
-                hour(Out_DepartureTime)+minute(Out_DepartureTime)/60 <= input$Leave_Dep_Time[2],
-                hour(Out_ArrivalTime)+minute(Out_ArrivalTime)/60 >= input$Leave_Arr_Time[1], 
-                hour(Out_ArrivalTime)+minute(Out_ArrivalTime)/60 <= input$Leave_Arr_Time[2],
-                Price <= input$price, 
-                CarrierName %in% input$Airline_In, 
-                !CarrierName %in% input$Airline_Ex)  %>% 
-         mutate(Out_DepartTime = Out_DepartureTime,
-                Out_Duration_Hr = round(Out_Duration/60, 1),
-                Price_USD = Price,
-                Link = sprintf('<a href="%s" target="_blank" class="btn btn-primary">Book</a>',LinkURL)) %>%
-         select(Price_USD, Out_DepartTime, Out_ArrivalTime, Out_Duration_Hr, Out_No.Stops,
-                CarrierName, Link)
-     }else{
-       # filter for round trip
-       dataset() %>% 
-         select(-c(Out_Stops, In_Stops)) %>% 
-         filter(Out_No.Stops <= input$Leave_Stops,
-                Out_Duration <= input$Leave_Duration * 60, 
-                hour(Out_DepartureTime)+minute(Out_DepartureTime)/60 >= input$Leave_Dep_Time[1], 
-                hour(Out_DepartureTime)+minute(Out_DepartureTime)/60 <= input$Leave_Dep_Time[2], 
-                hour(Out_ArrivalTime)+minute(Out_ArrivalTime)/60 >= input$Leave_Arr_Time[1], 
-                hour(Out_ArrivalTime)+minute(Out_ArrivalTime)/60 <= input$Leave_Arr_Time[2],
-                Price <= input$price, 
-                CarrierName %in% input$Airline_In,
-                !CarrierName %in% input$Airline_Ex) %>% 
-         filter(In_No.Stops <= input$Back_Stops,
-                In_Duration <= input$Back_Duration * 60,
-                hour(In_DepartureTime)+minute(In_DepartureTime)/60 >= input$Back_Dep_Time[1], 
-                hour(In_DepartureTime)+minute(In_DepartureTime)/60 <= input$Back_Dep_Time[2], 
-                hour(In_ArrivalTime)+minute(In_ArrivalTime)/60 >= input$Back_Arr_Time[1], 
-                hour(In_ArrivalTime)+minute(In_ArrivalTime)/60 <= input$Back_Arr_Time[2] ) %>%
-         mutate(Out_DepartTime = Out_DepartureTime,
-                Out_Duration_Hr = round(Out_Duration/60, 1),
-                In_DepartTime = In_DepartureTime,
-                In_Duration_Hr = round(In_Duration/60, 1),
-                Price_USD = Price,
-                Link = sprintf('<a href="%s" target="_blank" class="btn btn-primary">Book</a>',LinkURL)) %>% 
-         select(Price_USD, Out_DepartTime, Out_ArrivalTime, Out_Duration_Hr, Out_No.Stops,
-                In_DepartTime, In_ArrivalTime, In_Duration_Hr, In_No.Stops,
-                CarrierName, Link)
-     }
-   }, escape = FALSE)
+     dataset()$segment
+   }, escape = FALSE,options = list(pageLength =10))
    
   
   output$IATAtable <- renderDataTable({

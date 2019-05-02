d4807cd312d12c2582843bea0d2f598e08f4f107
qiaoyang00 <qiaoyang00@126.com>
2019-05-02 18:29:39 -0500

    Comment on task examples.

--- inst/task.R
+++ inst/task.R
@@ -1,21 +1,22 @@
 # library(taskscheduleR)  # Windows
 library(cronR)  # Unix/Linux
 
-rstudioapi::getPersistentValue
-rstudioapi::jobRunScript()
-rstudioapi::selectFile()
-rstudioapi::documentSave(1)
-rstudioapi::getActiveDocumentContext()
+# rstudioapi::getPersistentValue
+# rstudioapi::jobRunScript()
+# rstudioapi::selectFile()
+# rstudioapi::documentSave(1)
+# rstudioapi::getActiveDocumentContext()
+# 
+# Sys.info()["sysname"]
+# .Platform$OS.type
+# R.version$os
+# 
+# cron_rstudioaddin()
 
-Sys.info()["sysname"]
-.Platform$OS.type
-R.version$os
-
-cron_rstudioaddin()
-
+# Create cron job
 CreateJob("DSM", "DTW", "2019-06-01", path = file.path(getwd(), "inst"), frequency = "minutely")
-CreateJob("DSM", "DTW", "2019-06-01", path = file.path(getwd(), "inst"), frequency = "hourly")
-CreateJob("DSM", "DTW", "2019-06-01", path = file.path(getwd(), "inst"), frequency = "daily", at = "3AM")
+# CreateJob("DSM", "DTW", "2019-06-01", path = file.path(getwd(), "inst"), frequency = "hourly")
+# CreateJob("DSM", "DTW", "2019-06-01", path = file.path(getwd(), "inst"), frequency = "daily", at = "3AM")
 
 ## Get all the jobs
 cron_njobs()

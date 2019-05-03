ef4e212c2bf952daea81dc3dfabc8e2c4906e75a
dapengh <shenfeng9224@gmail.com>
2019-05-02 23:11:29 -0500

    update test function

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,12 +1,12 @@
 context("test-api")
 
 test_that("CreateSession function inputs are correct", {
-  expect_error(CreateSession(origin = 123, destination = "DSM", startDate = "2019-06-10"))
-  expect_error(CreateSession(origin = "DSM", destination = 123, startDate = "2019-06-10"))
-  expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019"))
-  expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", returnDate = "ISU"))
-  expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", returnDate = "2019-06-01"))
-  expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", adults = "4"))
+  expect_warning(CreateSession(origin = 123, destination = "DSM", startDate = "2019-06-10"))
+  expect_warning(CreateSession(origin = "DSM", destination = 123, startDate = "2019-06-10"))
+  expect_warning(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019"))
+  expect_warning(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", returnDate = "ISU"))
+  expect_warning(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", returnDate = "2019-06-01"))
+  expect_warning(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", adults = "as"))
 })
 
 test_that("CreateSession function output type is correct",{
@@ -19,5 +19,5 @@ test_that("PollSession function input sort type and order is correct",{
 })
 
 test_that("PollSession function output type is correct",{
-  expect_class(PollSession(), "response")
+  expect_class(PollSession(respondPOST = CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10")), "response")
 })

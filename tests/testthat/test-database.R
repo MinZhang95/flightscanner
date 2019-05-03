eecafcd35c3d57be1fba145fc9b34738b229df1e
zeruiz <zedray.chang@gmail.com>
2019-05-02 22:46:57 -0500

    Test test

--- tests/testthat/test-database.R
+++ tests/testthat/test-database.R
@@ -2,4 +2,17 @@ context("test-database")
 
 test_that("ListUnpack works", {
   expect_error(ListUnpack(x=2, mutate = TRUE))
+  res1 <- ListUnpack(iris, mutate = TRUE)
+  expect_true(is.list(res1))
+  res2 <- ListUnpack(iris, mutate = TRUE, vars = "Species")
+  expect_true(is.character(res2$Species))
 })
+
+test_that("ListPack works",{
+  expect_error(ListPack(x=2, mutate = TRUE))
+  res1 <- ListPack(iris, mutate = FALSE)
+  expect_true(is.numeric(res1))
+  res2 <- ListPack(iris, mutate = TRUE, vars = "Species")
+  expect_true(is.list(res2$Species))
+  }
+)

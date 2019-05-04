fb4b0762798d5cc88f73c12da2c64ed230d0e4c9
zeruiz <zedray.chang@gmail.com>
2019-05-04 13:59:11 -0500

    Test test

--- tests/testthat/test-database.R
+++ tests/testthat/test-database.R
@@ -1,8 +1,5 @@
 context("test-database")
 
-SetAPI(host = "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-       key = "fefb4945e2msh1d70bbb54d6ef9bp1901acjsn9d9be332a30e")
-
 test_that("ListUnpack works", {
   expect_error(ListUnpack(x = 2, mutate = TRUE))
   res1 <- ListUnpack(iris, mutate = TRUE)

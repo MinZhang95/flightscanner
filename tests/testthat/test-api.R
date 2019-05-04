9e95d3570b3d4e27ca710a290e0b5447bdb4c73e
zeruiz <zedray.chang@gmail.com>
2019-05-03 22:38:44 -0500

    Test test

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,21 +1,16 @@
 context("test-api")
 
-SetAPI(host = "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-       key = "a01b3ec5e9msh2698ef80ca5232dp18fc92jsnddf5fad0cc7d")
-
 test_that("CreateSession function inputs and outputs are correct", {
   expect_error(CreateSession(origin = 123, destination = "DSM", startDate = "2019-06-10"))
   expect_error(CreateSession(origin = "DSM", destination = 123, startDate = "2019-06-10"))
   expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", returnDate = "2019-06-01"))
   expect_error(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10", adults = "as"))
-  expect_s3_class(CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10"), "response")
 })
 
 
 test_that("PollSession function input sort type and order is correct", {
   expect_error(PollSession(sortType = "time"))
   expect_error(PollSession(sortOrder = "increase"))
-  expect_s3_class(PollSession(respondPOST = CreateSession(origin = "DSM", destination = "ORD", startDate = "2019-06-10")), "response")
 })
 
 

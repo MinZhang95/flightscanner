7ede93f150cec62cc23ac75527f1b5a1c2967712
qiaoyang00 <qiaoyang00@126.com>
2019-05-07 09:03:33 -0500

    Testing on codecov commit status.

--- R/schedule.R
+++ R/schedule.R
@@ -1,16 +1,14 @@
-#' Identify the operating system.
-#' @return A character of OS.
-get_os <- function() {
-  if (.Platform$OS.type == "windows") {
-    "win"
-  } else if (Sys.info()["sysname"] == "Darwin") {
-    "mac"
-  } else if (.Platform$OS.type == "unix") {
-    "unix"
-  } else {
-    stop("Unknown OS")
-  }
-}
+# get_os <- function() {
+#   if (.Platform$OS.type == "windows") {
+#     "win"
+#   } else if (Sys.info()["sysname"] == "Darwin") {
+#     "mac"
+#   } else if (.Platform$OS.type == "unix") {
+#     "unix"
+#   } else {
+#     stop("Unknown OS")
+#   }
+# }
 
 
 #' Convert missing arguments to NULL.
